a395ed87daf69e71b6b2a2018292c95485cad2e0
Jakub Pawlowski <jakub.pawlowski@guestline.com>
2020-05-20 07:29:36 +0200

    allow specifying custom schema path

--- src/schema-association-service.ts
+++ src/schema-association-service.ts
@@ -1,11 +1,13 @@
 /*---------------------------------------------------------------------------------------------
-*  Copyright (c) Microsoft Corporation. All rights reserved.
-*  Licensed under the MIT License.
-*--------------------------------------------------------------------------------------------*/
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License.
+ *--------------------------------------------------------------------------------------------*/
 
+import * as fs from 'fs';
 import * as path from 'path';
 import * as vscode from 'vscode';
 import * as languageclient from 'vscode-languageclient';
+import { log } from './logger';
 
 export interface ISchemaAssociationService {
     getSchemaAssociation(): ISchemaAssociations;
@@ -18,7 +20,30 @@ export class SchemaAssociationService implements ISchemaAssociationService {
     schemaFilePath: string;
 
     constructor(extensionPath: string) {
-        this.schemaFilePath = vscode.Uri.file(path.join(extensionPath, './service-schema.json')).toString();
+        let schemaPath = path.join(extensionPath, './service-schema.json');
+
+        let customPath: string = vscode.workspace
+            .getConfiguration()
+            .get('[azure-pipelines].schemaPath') as string;
+
+        if (customPath) {
+            if (!path.isAbsolute(customPath)) {
+                const files = vscode.workspace.workspaceFolders.map((ws) =>
+                    path.join(ws.uri.fsPath, customPath)
+                );
+                const foundSchema = files.find((f) => fs.existsSync(f));
+                if (foundSchema) customPath = foundSchema;
+            }
+
+            if (fs.existsSync(customPath)) {
+                log(`using cusom schema from '${customPath}'`);
+                schemaPath = customPath;
+            } else {
+                log(`schema file '${customPath}' not found`, "error");
+            }
+        }
+
+        this.schemaFilePath = vscode.Uri.file(schemaPath).toString();
     }
 
     public getSchemaAssociation(): ISchemaAssociations {
@@ -28,10 +53,10 @@ export class SchemaAssociationService implements ISchemaAssociationService {
 
 // TODO: Do we need this?
 export interface ISchemaAssociations {
-	[pattern: string]: string[];
+    [pattern: string]: string[];
 }
 
 // TODO: Do we need this?
 export namespace SchemaAssociationNotification {
-	export const type: languageclient.NotificationType<ISchemaAssociations, any> = new languageclient.NotificationType('json/schemaAssociations');
+    export const type: languageclient.NotificationType<ISchemaAssociations, any> = new languageclient.NotificationType('json/schemaAssociations');
 }
